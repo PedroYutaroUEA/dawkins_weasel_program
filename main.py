d45e98430f330b38554be018abcff494c0e335d7
YutaroUEA <pymmn.snf23@uea.edu.br>
2023-11-04 13:35:08 -0400

    testing pedro branch from local machine

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+def print_hi(user_name):
+    print(f'Hi, {user_name}')
 
 
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+name = 'pedro'
+age = 18
+
+if age >= 18:
+    print(f'{name} é maior de idade')
+else:
+    print(f'{name} é menor de idade')
